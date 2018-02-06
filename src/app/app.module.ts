af2b9cb3f43953a8b4bbe441d6fca5083c6ec9e7
joe <jaylor@topicdesign.com>
2018-02-06 14:06:21 -0500

    scaffolds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ControlsComponent } from './controls/controls.component';
+import { GridComponent } from './grid/grid.component';
+import { CellComponent } from './cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlsComponent,
+    GridComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule
